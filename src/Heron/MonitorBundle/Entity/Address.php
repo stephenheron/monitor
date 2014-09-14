f0a868b3de0b450c871c9ead37e93594e3665524
Stephen Heron <stephen.heron@gmail.com>
2014-09-14 14:17:30 +0100

    Added Address entity

---
+++ src/Heron/MonitorBundle/Entity/Address.php
@@ -0,0 +1,214 @@
+<?php
+
+namespace Heron\MonitorBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Address
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Heron\MonitorBundle\Entity\AddressRepository")
+ */
+class Address
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="company", type="string", length=255)
+     */
+    private $company;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="addressLine1", type="string", length=255)
+     */
+    private $addressLine1;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="addressLine2", type="string", length=255)
+     */
+    private $addressLine2;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="addressLine3", type="string", length=255)
+     */
+    private $addressLine3;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="town", type="string", length=255)
+     */
+    private $town;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="country", type="string", length=255)
+     */
+    private $country;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set company
+     *
+     * @param string $company
+     * @return Address
+     */
+    public function setCompany($company)
+    {
+        $this->company = $company;
+
+        return $this;
+    }
+
+    /**
+     * Get company
+     *
+     * @return string 
+     */
+    public function getCompany()
+    {
+        return $this->company;
+    }
+
+    /**
+     * Set addressLine1
+     *
+     * @param string $addressLine1
+     * @return Address
+     */
+    public function setAddressLine1($addressLine1)
+    {
+        $this->addressLine1 = $addressLine1;
+
+        return $this;
+    }
+
+    /**
+     * Get addressLine1
+     *
+     * @return string 
+     */
+    public function getAddressLine1()
+    {
+        return $this->addressLine1;
+    }
+
+    /**
+     * Set addressLine2
+     *
+     * @param string $addressLine2
+     * @return Address
+     */
+    public function setAddressLine2($addressLine2)
+    {
+        $this->addressLine2 = $addressLine2;
+
+        return $this;
+    }
+
+    /**
+     * Get addressLine2
+     *
+     * @return string 
+     */
+    public function getAddressLine2()
+    {
+        return $this->addressLine2;
+    }
+
+    /**
+     * Set addressLine3
+     *
+     * @param string $addressLine3
+     * @return Address
+     */
+    public function setAddressLine3($addressLine3)
+    {
+        $this->addressLine3 = $addressLine3;
+
+        return $this;
+    }
+
+    /**
+     * Get addressLine3
+     *
+     * @return string 
+     */
+    public function getAddressLine3()
+    {
+        return $this->addressLine3;
+    }
+
+    /**
+     * Set town
+     *
+     * @param string $town
+     * @return Address
+     */
+    public function setTown($town)
+    {
+        $this->town = $town;
+
+        return $this;
+    }
+
+    /**
+     * Get town
+     *
+     * @return string 
+     */
+    public function getTown()
+    {
+        return $this->town;
+    }
+
+    /**
+     * Set country
+     *
+     * @param string $country
+     * @return Address
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    /**
+     * Get country
+     *
+     * @return string 
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+}
