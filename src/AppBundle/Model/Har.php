ef9414136a5eadb50ea5682d056a39e9fe433b9f
Stephen Heron <stephen.heron@gmail.com>
2014-12-29 22:59:49 +0000

    Added some small convenience methods to the HAR model.

--- src/AppBundle/Model/Har.php
+++ src/AppBundle/Model/Har.php
@@ -33,7 +33,6 @@ class Har
         $rawHarArray = json_decode($harSource, true);
 
         if($rawHarArray){
-            dump($rawHarArray);
             $this->entries = $rawHarArray['log']['entries'];
 
         } else {
@@ -56,9 +55,31 @@ class Har
         return $this->mainRequest;
     }
 
+    public function getAllRequests()
+    {
+        return $this->getHtmlRequests() +
+            $this->getCSSRequests() +
+            $this->getJSRequests() +
+            $this->getImageRequests() +
+            $this->getVideoRequests() +
+            $this->getOtherRequests();
+    }
+
+    public function getAllRequestsSize()
+    {
+        $requests = $this->getAllRequests();
+        return $this->getResponseSize($requests);
+    }
+
     public function getHtmlRequests()
     {
         return $this->getRequests('HTML');
+    }
+
+    public function getHtmlRequestsSize()
+    {
+        $requests = $this->getHtmlRequests();
+        return $this->getResponseSize($requests);
     }
 
     public function getCSSRequests()
@@ -66,9 +87,21 @@ class Har
         return $this->getRequests('CSS');
     }
 
+    public function getCSSRequestsSize()
+    {
+        $requests = $this->getCSSRequests();
+        return $this->getResponseSize($requests);
+    }
+
     public function getJSRequests()
     {
         return $this->getRequests('JS');
+    }
+
+    public function getJSRequestsSize()
+    {
+        $requests = $this->getJSRequests();
+        return $this->getResponseSize($requests);
     }
 
     public function getImageRequests()
@@ -76,14 +109,32 @@ class Har
         return $this->getRequests('IMAGES');
     }
 
+    public function getImageRequestsSize()
+    {
+        $requests = $this->getImageRequests();
+        return $this->getResponseSize($requests);
+    }
+
     public function getVideoRequests()
     {
         return $this->getRequests('VIDEOS');
     }
 
+    public function getVideoRequestsSize()
+    {
+        $requests = $this->getVideoRequests();
+        return $this->getResponseSize($requests);
+    }
+
     public function getOtherRequests()
     {
         return $this->getRequests('OTHER');
+    }
+
+    public function getOtherRequestsSize()
+    {
+        $requests = $this->getVideoRequests();
+        return $this->getResponseSize($requests);
     }
 
     private function getRequests($type)
@@ -136,16 +187,33 @@ class Har
         $processedEntry['response_code'] = $entry['response']['status'];
         $processedEntry['response_headers'] = $entry['response']['headers'];
         $processedEntry['size'] = $entry['response']['content']['size'];
+
         if(isset($entry['response']['content']['compression'])) {
             $processedEntry['compressed_size'] = $entry['response']['content']['compression'];
         } else {
             $processedEntry['compressed_size'] = false;
         }
+
         $processedEntry['mime_type'] = $entry['response']['content']['mimeType'];
 
         return $processedEntry;
     }
 
+    private function getResponseSize($requests)
+    {
+        $size = 0;
+        $compressedSize = 0;
+
+        foreach($requests as $request) {
+            $size += $request['size'];
+            $compressedSize += $request['compressed_size'];
+        }
+
+        return [
+            'size'            => $size,
+            'compressed_size' => $compressedSize
+        ];
+    }
 
 
 }
